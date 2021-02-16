00e8cb2e209e7d83f8c54eaa8b307944247771dc
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-02-16 08:10:36 +0100

    shuffle questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,5 +1,6 @@
 import { Question } from "../types/Question";
-export const getQuestions = (): Question[] => [
+
+export const getQuestionsStraight = (): Question[] => [
   {
     topic : "Would you say it?",
     level : "easy",
@@ -17,16 +18,16 @@ export const getQuestions = (): Question[] => [
   {
     topic : "Would you say it?",
     level : "easy",
-    question : "Aber Männer werden doch auch diskriminiert",
+    question : "... aber Männer werden doch auch diskriminiert",
     answer : "no",
     explanation : "Diese Aussage folgt meist einer Problematik die z.B. Frauen betrifft (z.B. \"wir brauchen eine Frauenquote\") man relativiert dadurch aber das eigentlich Problem bzw. versucht vom eigentlichen Problem abzulenken. DAX notierte Unternehmen sind so groß, dass es quasi unmöglich ist, dass es keine qualifizierten Frauen gibt, die eine Führungsrolle im Vorstand übernehmen können (und das ist meist die Ausrede der Unternehmen warum es kaum Frauen im Vorstand/Management gibt) Mit der Quote kann man diese Unternehmen in die Pflicht nehmen, genauer hinzusehen. Sei es durch Ausbildungsprogramme (Female-Executive-Programs) oder ähnliches."
   },
   {
     topic : "Would you say it?",
     level : "easy",
-    question : "Frauen brauchen Ewigkeiten im Bad, Frauen reden viel, Frauen können nicht einparken, usw.",
+    question : "Frauen brauchen Ewigkeiten im Bad, reden zu viel, können nicht einparken, sind zu emotional usw.",
     answer : "no",
-    explanation : "Zunächst handelt es sich bei solchen Aussagen um eine Generalisierung die auf schlechter Beobachtung beruht. Es mag ja sein, dass man eine Frau kennt die z.B. lange im Bad braucht. Es gibt aber auch Frauen die schnell sind. Des weiteren arten solche Aussagen gerne in pauschales Frauenbashing aus, was nicht sehr nett ist. Du darfst kritisieren, aber bitte konkret und sachlich."
+    explanation : "Zunächst handelt es sich bei solchen Aussagen um eine Generalisierung die auf schlechter Beobachtung beruht. Es mag ja sein, dass man eine Frau kennt die z.B. lange im Bad braucht. Es gibt aber durchaus auch Frauen die schnell sind. Des Weiteren arten solche Aussagen gerne auch in pauschales Frauenbashing aus, was nicht sehr nett ist. Du darfst kritisieren, aber bitte konkret und sachlich."
   },
   {
     topic : "Would you say it?",
@@ -36,3 +37,20 @@ export const getQuestions = (): Question[] => [
     explanation : "Im Unternehmeskontext ist es schlicht und einfach unprofessionell: Es sind Frauen und keine Mädels. Du weißt zudem möglicherweise gar nicht, ob dort überhaupt nur Frauen sitzen. Du etablierst oder förderst gewisse veraltete, patriarchische Denkmuster."
   }
 ]
+
+export const getQuestions = (): Question[] => shuffleArray(getQuestionsStraight());
+
+function shuffleArray(array: Question[]) {
+  let curId = array.length;
+  // There remain elements to shuffle
+  while (0 !== curId) {
+    // Pick a remaining element
+    let randId = Math.floor(Math.random() * curId);
+    curId -= 1;
+    // Swap it with the current element.
+    let tmp = array[curId];
+    array[curId] = array[randId];
+    array[randId] = tmp;
+  }
+  return array;
+}
