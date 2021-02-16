7ab3a1c70d26c5b297002cb0f1185c3f81574809
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-02-16 14:08:18 +0100

    add new question

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -2,39 +2,46 @@ import { Question } from "../types/Question";
 
 export const getQuestionsStraight = (): Question[] => [
   {
-    topic : "Would you say it?",
+    topic : "Würdest du das so sagen?",
     level : "easy",
     question : "Du bist eine echte Powerfrau",
     answer : "no",
     explanation : "Man sollte immer davon ausgehen, dass Frauen genauso stark sind wie Männer. Männer werden von Haus aus nie als Power-Mann bezeichnet, man sollte also allen Menschen die gleichen Grundvoraussetzungen geben und auch nicht schon in Schubladen denken. Manche Menschen wollen auch selbst entscheiden ob und in welche Schublade sie gehören."
   },
   {
-    topic : "Would you say it?",
+    topic : "Würdest du das so sagen?",
     level : "easy",
     question : "Frauen haben keine Ahnung von Technik/IT",
     answer : "no",
     explanation : "Es gibt zwar weniger Frauen in technischen Berufen, dass liegt aber eher an der Gesellschaft als an Unverständnis. Bestimmte Begriffe werden oft schon von Kind auf bestimmten Geschlechtern zugeordnet. Sei es durch Sprache (DER Informatiker, DER Arzt, wenn man aber eigentlich Männer und Frauen meint) oder Vorleben (Mama bleibt zuhause, Papa geht arbeiten)."
   },
   {
-    topic : "Would you say it?",
+    topic : "Würdest du das so sagen?",
     level : "easy",
     question : "... aber Männer werden doch auch diskriminiert",
     answer : "no",
     explanation : "Diese Aussage folgt meist einer Problematik die z.B. Frauen betrifft (z.B. \"wir brauchen eine Frauenquote\") man relativiert dadurch aber das eigentlich Problem bzw. versucht vom eigentlichen Problem abzulenken. DAX notierte Unternehmen sind so groß, dass es quasi unmöglich ist, dass es keine qualifizierten Frauen gibt, die eine Führungsrolle im Vorstand übernehmen können (und das ist meist die Ausrede der Unternehmen warum es kaum Frauen im Vorstand/Management gibt) Mit der Quote kann man diese Unternehmen in die Pflicht nehmen, genauer hinzusehen. Sei es durch Ausbildungsprogramme (Female-Executive-Programs) oder ähnliches."
   },
   {
-    topic : "Would you say it?",
+    topic : "Würdest du das so sagen?",
     level : "easy",
     question : "Frauen brauchen Ewigkeiten im Bad, reden zu viel, können nicht einparken, sind zu emotional usw.",
     answer : "no",
     explanation : "Zunächst handelt es sich bei solchen Aussagen um eine Generalisierung die auf schlechter Beobachtung beruht. Es mag ja sein, dass man eine Frau kennt die z.B. lange im Bad braucht. Es gibt aber durchaus auch Frauen die schnell sind. Des Weiteren arten solche Aussagen gerne auch in pauschales Frauenbashing aus, was nicht sehr nett ist. Du darfst kritisieren, aber bitte konkret und sachlich."
   },
   {
-    topic : "Would you say it?",
+    topic : "Würdest du das so sagen?",
     level : "easy",
     question : "Frag doch mal bei den Mädels von HR/Marketing/CS nach",
     answer : "no",
     explanation : "Im Unternehmeskontext ist es schlicht und einfach unprofessionell: Es sind Frauen und keine Mädels. Du weißt zudem möglicherweise gar nicht, ob dort überhaupt nur Frauen sitzen. Du etablierst oder förderst gewisse veraltete, patriarchische Denkmuster."
+  },
+  {
+    topic : "Würdest du das so sagen?",
+    level : "easy",
+    question : "Frauen und Männer sind in unserer Gesellschaft gleichberechtigt und werden dementsprechend auch gleich behandelt.",
+    answer : "no",
+    explanation : "Es mag vielleicht manchmal den Anschein erwecken, dass Frauen und Männer überall gleich behandelt werden. Leider gibt es noch viele Bereiche in denen Frauen benachteiligt sind. Dazu gehören z.B. die Vergütung oder Karrieremöglichkeiten (auch wenn vielleicht der eigene Arbeitgeber hier schon sehr gut aufgestellt ist). Aber auch tief sitzender Sexismus oder veraltete Rollenbilder führen zu einer Benachteiligung, die Auswirkungen auf das gesamte Leben der Betroffenen haben."
   }
 ]
 
