ff9001bd478dc0e67c88f8c36b588057b2dfe1c2
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-02-16 19:49:33 +0100

    add new questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -4,16 +4,16 @@ export const getQuestionsStraight = (): Question[] => [
   {
     topic : "Würdest du das so sagen?",
     level : "easy",
-    question : "Du bist eine echte Powerfrau",
+    question : "Dein Vortrag hat mich echt vom Hocker gerissen. Du bist eine echte Powerfrau",
     answer : "no",
-    explanation : "Man sollte immer davon ausgehen, dass Frauen genauso stark sind wie Männer. Männer werden von Haus aus nie als Power-Mann bezeichnet, man sollte also allen Menschen die gleichen Grundvoraussetzungen geben und auch nicht schon in Schubladen denken. Manche Menschen wollen auch selbst entscheiden ob und in welche Schublade sie gehören."
+    explanation : "Man sollte immer davon ausgehen, dass Frauen genauso stark sind wie Männer. Männer werden von Haus aus nie als Power-Mann bezeichnet, man sollte also allen Menschen die gleichen Grundvoraussetzungen geben und auch nicht schon in Schubladen denken. Manche Menschen wollen auch selbst entscheiden ob und in welche Schublade sie gehören. Dass einen der Vortrag vom Hocker gerissen hat ist doch auch schon ein schönes Lob und ganz ohne Schubladen."
   },
   {
     topic : "Würdest du das so sagen?",
     level : "easy",
-    question : "Frauen haben keine Ahnung von Technik/IT",
+    question : "Frauen haben doch keine Ahnung von Technik oder IT",
     answer : "no",
-    explanation : "Es gibt zwar weniger Frauen in technischen Berufen, dass liegt aber eher an der Gesellschaft als an Unverständnis. Bestimmte Begriffe werden oft schon von Kind auf bestimmten Geschlechtern zugeordnet. Sei es durch Sprache (DER Informatiker, DER Arzt, wenn man aber eigentlich Männer und Frauen meint) oder Vorleben (Mama bleibt zuhause, Papa geht arbeiten)."
+    explanation : "Es gibt zwar weniger Frauen in technischen Berufen, dass liegt aber eher an der Gesellschaft als an Unverständnis. Bestimmte Begriffe werden oft schon von Kind auf bestimmten Geschlechtern zugeordnet. Sei es durch Sprache (DER Informatiker, DER Arzt, wenn man aber eigentlich alle Geschlechter meint) oder Vorleben (Mama bleibt zuhause, Papa geht arbeiten)."
   },
   {
     topic : "Würdest du das so sagen?",
@@ -42,6 +42,20 @@ export const getQuestionsStraight = (): Question[] => [
     question : "Frauen und Männer sind in unserer Gesellschaft gleichberechtigt und werden dementsprechend auch gleich behandelt.",
     answer : "no",
     explanation : "Es mag vielleicht manchmal den Anschein erwecken, dass Frauen und Männer überall gleich behandelt werden. Leider gibt es noch viele Bereiche in denen Frauen benachteiligt sind. Dazu gehören z.B. die Vergütung oder Karrieremöglichkeiten (auch wenn vielleicht der eigene Arbeitgeber hier schon sehr gut aufgestellt ist). Aber auch tief sitzender Sexismus oder veraltete Rollenbilder führen zu einer Benachteiligung, die Auswirkungen auf das gesamte Leben der Betroffenen haben."
+  },
+  {
+    topic : "Würdest du das so sagen?",
+    level : "easy",
+    question : "(zu Kollegin nach ihrem Vortrag) Ich fand deinen Vortrag wirklich super",
+    answer : "yes",
+    explanation : "Niemand hat etwas gegen ein Lob oder ein paar nette Worte, wenn es ehrlich gemeint ist. Egal ob es von einem Mann oder einer Frau kommt."
+  },
+  {
+    topic : "Würdest du das so sagen?",
+    level : "easy",
+    question : "Sprache zu gendern ist ein wichtiger Schritt in Richtung der Gleichstellung von Mann und Frau",
+    answer : "yes",
+    explanation : "Die ständige Nutzung des generischen Maskulins zeichnet (wenn auch unterbewusst) ein gewisses Bild in unseren Köpfen. So wird z.B. \"der Arzt\" automatisch als Mann interpretiert, auch wenn damit eine Frau gemeint sein könnte. Um diese Ausgrenzung anderer Geschlechter zu vermeiden, sollte man auf alternative, inkludierende Ausdrucksweisen ausweichen."
   }
 ]
 
